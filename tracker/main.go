8036fe57a3095ba9d53042f26cf8f3b0908aa43f
TinoIf <tinoinformatics@gmail.com>
2025-09-24 11:52:58 +0700

    "Feat-add : Menambahkan Tampilan Program dan Input dan Juga Logika Menu Program untuk Menjalankan Masing-Masing Fungsi yang sudah Diimport dari Package Transaction"

--- tracker/main.go
+++ tracker/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"exspense-tracker/transaction"
+	"fmt"
+)
+
 func main() {
+	var pilihan int
+
+	fmt.Println("================================= Exspense Tracker =================================")
+	fmt.Println("================================= Pilihlah Menu    =================================")
+	fmt.Println("1. Masukkan Transaksi")
+	fmt.Println("2. Tampilkan Semua Catatan Transaksi")
+	fmt.Println("3. Hapus Transaksi")
+	fmt.Println("4. Keluar Program")
+	fmt.Print("Masukkan Nilai yang diPilih : ")
+	fmt.Scan(&pilihan)
+
+	for {
+		if pilihan == 1 {
+			transaction.Laporan = transaction.AddTransaction(transaction.Laporan)
+		}
+		if pilihan == 2 {
+			transaction.DisplayTransaction(transaction.Laporan)
+		}
+		if pilihan == 3 {
+			//Ini Pilihan 3 Untuk Menghapus Salah Satu Transaksi
+		}
+		if pilihan == 4 {
+			fmt.Println("=========== Terimakasih Telah Menggunakan Program =============")
+			break
+		}
+	}
 
 }
