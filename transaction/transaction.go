e62f0948df0a76311800fb468959579c8a121f76
TinoIf <tinoinformatics@gmail.com>
2025-09-24 10:19:40 +0700

    "feat-add: Membuat Function untuk Menambahkan Laporan Transaksi"

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,5 +1,13 @@
 package transaction
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Transaction struct {
 	Deskripsi string
 	Harga     float64
@@ -7,3 +15,31 @@ type Transaction struct {
 }
 
 var Laporan = []Transaction{}
+
+func AddTransaction(Lapor []Transaction) []Transaction {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Masukkan Deskripsi Pengeluaran atau Pemasukkan : ")
+	deskripsi, _ := reader.ReadString('\n')
+	deskripsi = strings.TrimSpace(deskripsi)
+
+	fmt.Println("Masukkan Harga : ")
+	inputHarga, _ := reader.ReadString('\n')
+	harga, err := strconv.ParseFloat(inputHarga, 64)
+
+	if err != nil {
+		fmt.Println("Konversi Harga Gagal Dilakukan, Masukkan Harga tanpa huruf")
+	}
+
+	fmt.Println("Masukkan Jenis nya Ketik Pengeluaran / Pemasukkan")
+	tipe, _ := reader.ReadString('\n')
+
+	olahLaporan := Transaction{
+		Deskripsi: deskripsi,
+		Harga:     harga,
+		Tipe:      tipe,
+	}
+
+	Lapor = append(Lapor, olahLaporan)
+
+	return Lapor
+}
