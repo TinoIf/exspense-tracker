05e5adbbadc0b9926651af570bff16e93c6fbde8
TinoIf <tinoinformatics@gmail.com>
2025-09-24 15:18:49 +0700

    "Feat-add : Menambahkan Fungsi untuk Menghapus Transaksi yang Ada"

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -18,20 +18,22 @@ var Laporan = []Transaction{}
 
 func AddTransaction(Lapor []Transaction) []Transaction {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Masukkan Deskripsi Pengeluaran atau Pemasukkan : ")
+	fmt.Print("Masukkan Deskripsi Transaksi: ")
 	deskripsi, _ := reader.ReadString('\n')
 	deskripsi = strings.TrimSpace(deskripsi)
 
-	fmt.Println("Masukkan Harga : ")
+	fmt.Print("Masukkan Harga : ")
 	inputHarga, _ := reader.ReadString('\n')
+	inputHarga = strings.TrimSpace(inputHarga)
 	harga, err := strconv.ParseFloat(inputHarga, 64)
 
 	if err != nil {
-		fmt.Println("Konversi Harga Gagal Dilakukan, Masukkan Harga tanpa huruf")
+		fmt.Println("Konversi Harga Gagal Dilakukan, Masukkan Angka tanpa huruf")
 	}
 
-	fmt.Println("Masukkan Jenis nya Ketik Pengeluaran / Pemasukkan")
+	fmt.Print("Masukkan Jenis nya Ketik Pengeluaran / Pemasukkan : ")
 	tipe, _ := reader.ReadString('\n')
+	tipe = strings.TrimSpace(tipe)
 
 	olahLaporan := Transaction{
 		Deskripsi: deskripsi,
@@ -49,6 +51,34 @@ func DisplayTransaction(Lapor []Transaction) {
 	fmt.Println("======================== Berikut Laporan Transaksi Kamu ==============================")
 	fmt.Println("No\tDeskripsi\t\tHarga\t\tTipe")
 	for index, value := range Lapor {
-		fmt.Print(index+1, "\t", value.Deskripsi, "\t\t", value.Harga, "\t", value.Tipe)
+		fmt.Print(index+1, "\t", value.Deskripsi, "\t\t", value.Harga, "\t\t", value.Tipe)
 	}
+}
+
+func DeleteTransaction(Lapor []Transaction) []Transaction {
+
+	var deletedId int
+	var indexDeleted int
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Masukkan Index Yang Ingin Di Hapus : ")
+	deletedIdS, _ := reader.ReadString('\n')
+	deletedIdS = strings.TrimSpace(deletedIdS)
+	deletedId, _ = strconv.Atoi(deletedIdS)
+	fmt.Printf("Index nya adalah %d", deletedId)
+
+	if deletedId < 0 || deletedId > len(Lapor) {
+		fmt.Println("Maaf Index Yang Kamu Masukan Tidak ada dan Tidak Valid")
+		return Lapor
+	}
+
+	for index := range Lapor {
+		if index == deletedId {
+			indexDeleted = index - 1
+			break
+		}
+	}
+
+	Lapor = append(Lapor[:indexDeleted], Lapor[indexDeleted+1:]...)
+	return Lapor
+
 }
