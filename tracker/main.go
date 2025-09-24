f415f0052a75ac30450b273a99b36e445c249698
TinoIf <tinoinformatics@gmail.com>
2025-09-24 15:19:49 +0700

    "Feat- add Menambahkan Logic Memanggil Fungsi Menghapus dan Memperbaiki Eror Input Akibat Enter"

--- tracker/main.go
+++ tracker/main.go
@@ -1,33 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"exspense-tracker/transaction"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	var pilihan int
-
-	fmt.Println("================================= Exspense Tracker =================================")
-	fmt.Println("================================= Pilihlah Menu    =================================")
-	fmt.Println("1. Masukkan Transaksi")
-	fmt.Println("2. Tampilkan Semua Catatan Transaksi")
-	fmt.Println("3. Hapus Transaksi")
-	fmt.Println("4. Keluar Program")
-	fmt.Print("Masukkan Nilai yang diPilih : ")
-	fmt.Scan(&pilihan)
-
+	var pilihan string
 	for {
-		if pilihan == 1 {
+		fmt.Println()
+		fmt.Println("================================= Exspense Tracker =================================")
+		fmt.Println("================================= Pilihlah Menu    =================================")
+		fmt.Println("1. Masukkan Transaksi")
+		fmt.Println("2. Tampilkan Semua Catatan Transaksi")
+		fmt.Println("3. Hapus Transaksi")
+		fmt.Println("4. Keluar Program")
+		fmt.Print("Masukkan Nilai yang diPilih : ")
+		reader := bufio.NewReader(os.Stdin)
+		pilihan, _ = reader.ReadString('\n')
+		pilihan = strings.TrimSpace(pilihan)
+		pilihanMenu, _ := strconv.Atoi(pilihan)
+
+		if pilihanMenu == 1 {
 			transaction.Laporan = transaction.AddTransaction(transaction.Laporan)
+			continue
 		}
-		if pilihan == 2 {
+		if pilihanMenu == 2 {
 			transaction.DisplayTransaction(transaction.Laporan)
+			continue
 		}
-		if pilihan == 3 {
-			//Ini Pilihan 3 Untuk Menghapus Salah Satu Transaksi
+		if pilihanMenu == 3 {
+			transaction.Laporan = transaction.DeleteTransaction(transaction.Laporan)
+			continue
 		}
-		if pilihan == 4 {
+		if pilihanMenu == 4 {
 			fmt.Println("=========== Terimakasih Telah Menggunakan Program =============")
 			break
 		}
